9bf45a0d3b077ed16813c146d9eb9eab948ce0ce
Marcos Díaz-Gay <mdiazgay@ucsd.edu>
2022-12-31 10:39:43 +0100

    Update test and travis latex

--- test.R
+++ test.R
@@ -1,18 +1,4 @@
 library(devtools)
 install_github("AlexandrovLab/SigProfilerMatrixGeneratorR@development")
 
-library(reticulate)
-py_config()
-
 library(SigProfilerMatrixGeneratorR)
-
-cnv <- CNVMatrixGenerator("BATTENBERG",
-                          "./test_data/CNV/Battenberg_test.tsv",
-                          "test_CNV",
-                          "./test_data/output_CNV")
-
-
-sv <- SVMatrixGenerator("./test_data/SV",
-                          "test_CNV",
-                          "./test_data/output_SV")
-
