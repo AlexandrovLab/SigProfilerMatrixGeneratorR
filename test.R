aef655de9bc1e64a4fa6d01ebdf8c3a6e47143e2
Marcos Díaz-Gay <mdiazgay@ucsd.edu>
2022-12-31 10:28:57 +0100

    Add cnv & sv tests

--- test.R
+++ test.R
@@ -1,3 +1,18 @@
 library(devtools)
-devtools::install_github("AlexandrovLab/SigProfilerMatrixGeneratorR@development")
+install_github("AlexandrovLab/SigProfilerMatrixGeneratorR@development")
+
+library(reticulate)
+py_config()
+
 library(SigProfilerMatrixGeneratorR)
+
+cnv <- CNVMatrixGenerator("BATTENBERG",
+                          "./test_data/CNV/Battenberg_test.tsv",
+                          "test_CNV",
+                          "./test_data/output_CNV")
+
+
+sv <- SVMatrixGenerator("./test_data/SV",
+                          "test_CNV",
+                          "./test_data/output_SV")
+
